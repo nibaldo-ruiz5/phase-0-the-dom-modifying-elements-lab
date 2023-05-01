ddd98c37550c1ebd3276c44c9f8ef9d2575af4f4
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-04-30 20:14:09 -0400

    with your message here

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const championName = "YOUR-NAME";
+const newHeader = document.createElement('h1');
+newHeader.textContent = `${championName} is the champion`;
+newHeader.id = "victory";
+
+const main = document.querySelector('main#main');
+main.remove();
+
+
+
